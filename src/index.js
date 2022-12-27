1043659de1998d983a7365473fe82c9a3006423a
developersung13 <developersung13@gmail.com>
2022-12-27 20:42:46 +0900

    [#1 PURE REDUX: COUNTER]: #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ import { legacy_createStore } from "redux";
 const cntText = document.getElementById("cnt");
 const incBtn = document.getElementById("inc");
 const decBtn = document.getElementById("dec");
-const tmpBtn = document.getElementById("asd");
 
-const countModifier = (cnt = 0) => {
+const incCntHandler = () => {
+  countStore.dispatch({ type: "INCREMENT" });
+  cntText.innerText = countStore.getState();
+};
+
+const decCntHandler = () => {
+  countStore.dispatch({ type: "DECREMENT" });
+  cntText.innerText = countStore.getState();
+};
+
+const countModifier = (cnt = 0, action) => {
+  console.log(cnt, action);
+  switch (action.type) {
+    case "INCREMENT":
+      return cnt + 1;
+      break;
+    case "DECREMENT":
+      return cnt - 1;
+      break;
+    default:
+      return cnt;
+  }
   return cnt;
 };
 
 const countStore = legacy_createStore(countModifier);
 
-console.log(countStore.getState());
+incBtn.addEventListener("click", incCntHandler);
+decBtn.addEventListener("click", decCntHandler);
