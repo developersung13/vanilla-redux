30cb9fbb6dc59cf6bce72eebdd72d8cd6506e55e
developersung13 <developersung13@gmail.com>
2023-01-15 17:44:26 +0900

    Refactoring (store.js) file

--- src/store.js
+++ src/store.js
@@ -20,7 +20,7 @@ export const deleteToDo = () => {
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      return [{ text: action.text, id: state.length }, ...state];
     case DELETE:
       return state.filter((toDo) => toDo !== action.id);
     default:
